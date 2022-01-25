d4114cf265a77207b5965f71602afe4ee2b0da03
✔️Martin Hartfelder <66142507+TheMartinfer22@users.noreply.github.com>
2022-01-24 22:27:48 -0300

    ✅ Link para os gerenciadores (tempo, config).

--- src/main/java/dev/nanosync/viputilities/commands/GeneralVIPCommands.java
+++ src/main/java/dev/nanosync/viputilities/commands/GeneralVIPCommands.java
@@ -1,19 +1,16 @@
 package dev.nanosync.viputilities.commands;
 
 import dev.nanosync.viputilities.api.LuckPermsAPI;
+import dev.nanosync.viputilities.manager.ConfigManager;
+import dev.nanosync.viputilities.manager.TimeManager;
 import dev.nanosync.viputilities.service.VIPGroupService;
 import me.saiintbrisson.minecraft.command.annotation.Command;
 import me.saiintbrisson.minecraft.command.command.Context;
-import net.luckperms.api.LuckPermsProvider;
-import net.luckperms.api.model.data.NodeMap;
-import org.bukkit.Bukkit;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.ConsoleCommandSender;
 
-import java.time.Duration;
-
-public class VIPManagerCommands {
+public class GeneralVIPCommands {
 
     LuckPermsAPI luckPermsAPI = new LuckPermsAPI();
 
@@ -40,15 +37,13 @@ public class VIPManagerCommands {
         if (isAdmin(commandSender)){
             if (context.argsCount() == 1){
                 String playerNickname = context.getArg(0);
-                commandSender.sendMessage("O vip de " + playerNickname + " vai expirar em " + getExpiryTime(Bukkit.getPlayer(playerNickname)));
+                commandSender.sendMessage(getConfigExpiryTime(playerNickname, "ExpiryVIPOtherMessage"));
                 return;
             }
-            if (isVIP(commandSender)) commandSender.sendMessage("Vai expirar em " + getExpiryTime(commandSender));
-            return;
         }
 
         if (isVIP(commandSender)){
-            commandSender.sendMessage("Vai expirar em " + getExpiryTime(commandSender));
+            commandSender.sendMessage(getConfigExpiryTime(commandSender.getName(), "ExpiryVIPMessage"));
             return;
         }
 
@@ -60,22 +55,22 @@ public class VIPManagerCommands {
         CommandSender commandSender = (CommandSender) context.getSender();
         if (isAdmin(commandSender)){
             if (context.argsCount() < 1){
-                commandSender.sendMessage("Você deve especificar o jogador, cargo e quantidade de dias!");
+                commandSender.sendMessage(ConfigManager.getConfigMessage("ErrorSpecifyPlayerGroupDaysAdd"));
                 return;
             }
 
             if (context.argsCount() < 2){
-                commandSender.sendMessage("Você deve especificar o cargo!");
+                commandSender.sendMessage(ConfigManager.getConfigMessage("ErrorSpecifyGroup"));
                 return;
             }
 
             if (context.argsCount() < 3){
-                commandSender.sendMessage("Você deve especificar os dias!");
+                commandSender.sendMessage(ConfigManager.getConfigMessage("ErrorSpecifyDays"));
                 return;
             }
 
             luckPermsAPI.addPlayerVIP(context.getArg(0), context.getArg(1), context.getArg(2, Integer.class));
-            commandSender.sendMessage("Adicionado com sucesso");
+            commandSender.sendMessage(ConfigManager.getConfigMessage("SuccessAdded"));
             return;
         }
         sendNoPermission(commandSender);
@@ -86,27 +81,20 @@ public class VIPManagerCommands {
         CommandSender commandSender = (CommandSender) context.getSender();
         if (isAdmin(commandSender)){
             if (context.argsCount() < 1){
-                commandSender.sendMessage("Você deve especificar o jogador e o cargo!");
+                commandSender.sendMessage(ConfigManager.getConfigMessage("ErrorSpecifyPlayerGroupRemove"));
                 return;
             }
 
             if (context.argsCount() < 2){
-                commandSender.sendMessage("Você deve especificar o cargo!");
+                commandSender.sendMessage(ConfigManager.getConfigMessage("ErrorSpecifyGroup"));
                 return;
             }
 
             luckPermsAPI.removePlayerVIP(context.getArg(0), context.getArg(1));
-            commandSender.sendMessage("Removido com sucesso");
+            commandSender.sendMessage(ConfigManager.getConfigMessage("SuccessRemoved"));
             return;
         }
         sendNoPermission(commandSender);
-    }
-
-    @Command(name = "debugvip")
-    public void dev(Context<CommandExecutor> context){
-        CommandSender commandSender = (CommandSender) context.getSender();
-        NodeMap data = LuckPermsProvider.get().getUserManager().getUser(commandSender.getName()).data();
-        data.toCollection().forEach(System.out::println);
     }
 
     private boolean isAdmin(CommandSender commandSender){
@@ -118,10 +106,11 @@ public class VIPManagerCommands {
     }
 
     private void sendNoPermission(CommandSender commandSender){
-        commandSender.sendMessage("you are not permission to execute this!");
+        commandSender.sendMessage(ConfigManager.getConfigMessage("ErrorNoPermission"));
     }
 
-    private Duration getExpiryTime(CommandSender commandSender){
-        return luckPermsAPI.getExpiryTime(commandSender, luckPermsAPI.getPlayerGroup(commandSender, VIPGroupService.getVIPGroups()));
+    private String getConfigExpiryTime(String player, String expiryConfig){
+        TimeManager timeManager = new TimeManager(luckPermsAPI.getExpiryTime(player, luckPermsAPI.getPlayerGroup(player, VIPGroupService.getVIPGroups())));
+        return ConfigManager.getMessageByConfigExpiry(expiryConfig, player, timeManager.getDays(), timeManager.getHours(), timeManager.getMinutes());
     }
 }
