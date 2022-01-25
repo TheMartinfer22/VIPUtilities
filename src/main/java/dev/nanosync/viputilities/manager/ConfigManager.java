40615e05a7390d293ecb801278f32862f0ebda05
✔️Martin Hartfelder <66142507+TheMartinfer22@users.noreply.github.com>
2022-01-24 22:25:36 -0300

    ⚙️Classe responsável por gerenciar as configurações.

---
+++ src/main/java/dev/nanosync/viputilities/manager/ConfigManager.java
@@ -0,0 +1,28 @@
+package dev.nanosync.viputilities.manager;
+
+import dev.nanosync.viputilities.VIPUtilities;
+
+public class ConfigManager {
+    public static String getConfigMessage(String configPath){
+        return VIPUtilities.getInstance().getConfig().getString(configPath).replace("&", "§");
+    }
+
+    public static String getMessageByConfigExpiry(String configPath, String playername, long days, long hours, long minutes) {
+        return VIPUtilities.getInstance().getConfig().getString(configPath)
+                .replace("{player}", playername)
+                .replace("{days}", days + " " + getTimeString(days, "DAYS"))
+                .replace("{hours}", hours + " " + getTimeString(hours, "HOURS"))
+                .replace("{minutes}", minutes + " " + getTimeString(minutes, "MINUTES"))
+                .replace("&", "§");
+    }
+
+    private static String getTimeString(long time, String path){
+        String timeString = getConfigMessage(path);
+        if (time == 1){
+            timeString = timeString.substring(0, timeString.length() - 1);
+            return timeString;
+        }
+        return timeString;
+    }
+
+}
