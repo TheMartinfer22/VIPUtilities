5fe7fca06098fb30bafc3d65601e1d4aed716fa5
✔️Martin Hartfelder <66142507+TheMartinfer22@users.noreply.github.com>
2022-01-25 23:42:22 -0300

    🔨 Init para o DiscordAPI

--- src/main/java/dev/nanosync/viputilities/VIPUtilities.java
+++ src/main/java/dev/nanosync/viputilities/VIPUtilities.java
@@ -1,21 +1,32 @@
 package dev.nanosync.viputilities;
 
+import dev.nanosync.viputilities.api.DiscordAPI;
 import dev.nanosync.viputilities.commands.GeneralVIPCommands;
 import me.saiintbrisson.bukkit.command.BukkitFrame;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class VIPUtilities extends JavaPlugin {
 
+    private static final DiscordAPI discordAPI = new DiscordAPI();
+
     @Override
     public void onEnable() {
         saveDefaultConfig();
         BukkitFrame frame = new BukkitFrame(this);
         frame.registerCommands(new GeneralVIPCommands());
+
+        if (!getConfig().getString("DiscordToken").isEmpty()) {
+            discordAPI.build(getConfig().getString("DiscordToken"), getConfig().getString("GuildID"));
+        }
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    public DiscordAPI getDiscordAPI(){
+        return discordAPI;
     }
 
     public static VIPUtilities getInstance(){
