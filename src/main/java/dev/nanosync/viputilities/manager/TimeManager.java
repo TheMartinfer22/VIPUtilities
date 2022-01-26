5667222ef208c1125b39aac6d96bf5b726af6583
✔️Martin Hartfelder <66142507+TheMartinfer22@users.noreply.github.com>
2022-01-25 23:41:48 -0300

    🔨 Nova formula de conversão de tempo

--- src/main/java/dev/nanosync/viputilities/manager/TimeManager.java
+++ src/main/java/dev/nanosync/viputilities/manager/TimeManager.java
@@ -9,23 +9,14 @@ public class TimeManager {
     }
 
     public long getDays(){
-        if (seconds >= 86400) return (seconds / 86400);
-        return 0;
+        return seconds/60/60/24;
     }
 
     public long getHours(){
-        if (seconds >= 3600) {
-            if (seconds / 3600 > 24) return 24;
-            return (seconds / 3600);
-        }
-        return 0;
+        return (seconds/60/60) % 24;
     }
 
     public long getMinutes(){
-        if (seconds >= 60) {
-            if (seconds / 60 > 60) return 60;
-            return (seconds / 60);
-        }
-        return 0;
+        return (seconds/60) % 60;
     }
 }
