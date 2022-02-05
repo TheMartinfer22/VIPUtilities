8af7a585c76010956736804117bed6d5a7b7cb40
Martinfer22 <martinsocial1706@gmail.com>
2022-02-05 19:43:41 -0300

    🔨 Correção de Uppercasing

--- src/main/java/dev/nanosync/viputilities/service/DiscordService.java
+++ src/main/java/dev/nanosync/viputilities/service/DiscordService.java
@@ -1,16 +1,18 @@
 package dev.nanosync.viputilities.service;
 
+import dev.nanosync.nanoapi.external.NanoDiscord;
 import dev.nanosync.viputilities.VIPUtilities;
-import dev.nanosync.viputilities.api.DiscordAPI;
 import dev.nanosync.viputilities.manager.ConfigManager;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.TextChannel;
 
 import java.awt.*;
+import java.util.Locale;
+import java.util.Objects;
 
 public class DiscordService {
 
-    private static final DiscordAPI discord = VIPUtilities.getInstance().getDiscordAPI();
+    private static final NanoDiscord discord = VIPUtilities.getDiscord();
     private static final String channel = ConfigManager.getConfigMessage("ChannelID");
 
     public static void announceNewVip(String player, String group){
@@ -18,8 +20,8 @@ public class DiscordService {
         embed.setColor(Color.YELLOW);
         embed.setDescription(ConfigManager.getConfigMessage("AnnounceNewVipMessage")
                 .replace("{player}", player)
-                .replace("{group}", group));
-        TextChannel channel = discord.getGuild().getTextChannelById(DiscordService.channel);
-        channel.sendMessageEmbeds(embed.build()).queue();
+                .replace("{group}", group.toUpperCase(Locale.ROOT)));
+        TextChannel channel = Objects.requireNonNull(discord).getGuild().getTextChannelById(DiscordService.channel);
+        Objects.requireNonNull(channel).sendMessageEmbeds(embed.build()).queue();
     }
 }
